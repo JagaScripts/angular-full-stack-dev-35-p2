5525b3c9bc761dc3cc542101593ca949c0d2bd2e
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-06-09 22:49:36 +0200

    Preparado para testear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Correo } from './correo/correo.modelo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-full-stack-dev-u35-2';
+
+  formReactive: FormGroup;
+  correos: Correo[] = [];
+  correo: Correo = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+  };
+
+  preguntasecreta: string = '';
+
+  constructor(private formBuilder: FormBuilder){
+    this.formReactive = this.formBuilder.group({
+      nombre: ['',Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', Validators.required],
+      preguntasecreta: ['', Validators.required],
+    });
+  }
+
+  enviarCorreo() {
+
+    if(this.formReactive.valid){
+
+      let auxCorreo = new Correo(this.correo.nombre,this.correo.email,this.correo.mensaje);
+
+      this.correos.push(auxCorreo);
+
+      this.onResetForm();
+
+    }
+
+  }
+
+  onResetForm() {
+    this.formReactive.reset();
+  }
+
+  getValue(value: string) {
+
+    return this.formReactive.get(value); //retorna todo el control
+  }
+
+  validarPreguntaSecreta() {
+    if (this.preguntasecreta === '5') {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
